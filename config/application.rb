44c3f2302f5d07071ed30f33a101451a473707ef
paranoiase Kang <paranoiase@gmail.com>
2012-04-09 18:21:04 +0900

    modify localization of will_paginate

--- config/application.rb
+++ config/application.rb
@@ -31,7 +31,7 @@ module Mintoffice
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', 'ko', '*.yml')]
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.yml')]
     config.i18n.default_locale = :ko
 
     # Configure the default encoding used in templates for Ruby 1.9.
