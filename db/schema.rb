3112fdc51d7987d6f6b9bc789f252160e7d5fe62
paranoiase Kang <paranoiase@gmail.com>
2012-04-09 17:03:08 +0900

    temporally fix bug

--- db/schema.rb
+++ db/schema.rb
@@ -179,8 +179,8 @@ ActiveRecord::Schema.define(:version => 20120405082459) do
     t.datetime "updated_at", :null => false
   end
 
-  create_table p, :force => true do |t|
-    t.string   "name"]
+  create_table "tags", :force => true do |t|
+    t.string   "name"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
