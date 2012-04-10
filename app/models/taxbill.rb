1bc898e9e271880168a2d97ce36bb1cb7fe72e86
paranoiase Kang <paranoiase@gmail.com>
2012-04-10 15:29:25 +0900

    add taxman to taxbill

--- app/models/taxbill.rb
+++ app/models/taxbill.rb
@@ -1,8 +1,17 @@
+# encoding: UTF-8
+
 class Taxbill < ActiveRecord::Base
   belongs_to :taxman
   has_many :taxbill_items, :dependent => :destroy
 
   BILL_TYPE = [:purchase, :sale]
+
+  def self.taxmen_list
+    Taxman.all.map do |taxman|
+      ["#{taxman.fullname} / #{taxman.business_client.name}", taxman.id]
+    end
+  end
+
   def price
     100
   end
