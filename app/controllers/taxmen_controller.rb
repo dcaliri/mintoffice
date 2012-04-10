7a3cb96e671c5b496533fc86fd4ec0da07335134
paranoiase Kang <paranoiase@gmail.com>
2012-04-10 12:10:21 +0900

    apply taxman

---
+++ app/controllers/taxmen_controller.rb
@@ -0,0 +1,38 @@
+class TaxmenController < ApplicationController
+  before_filter :find_business_client
+
+  def new
+    @taxman = @business_client.taxmen.new
+  end
+
+  def create
+    @taxman = @business_client.taxmen.build(params[:taxman])
+    @taxman.save!
+    redirect_to @business_client
+  rescue ActiveRecord::RecordInvalid
+    render 'new'
+  end
+
+  def edit
+    @taxman = @business_client.taxmen.find(params[:id])
+  end
+
+  def update
+    @taxman = @business_client.taxmen.find(params[:id])
+    @taxman.update_attributes(params[:taxman])
+    redirect_to @business_client
+  rescue ActiveRecord::RecordInvalid
+    render 'edit'
+  end
+
+  def destroy
+    @taxman = @business_client.taxmen.find(params[:id])
+    @taxman.destroy
+    redirect_to @business_client
+  end
+
+  private
+  def find_business_client
+    @business_client = BusinessClient.find(params[:business_client_id])
+  end
+end
--- app/models/taxman.rb
+++ app/models/taxman.rb
@@ -1,3 +1,5 @@
 class Taxman < ActiveRecord::Base
   belongs_to :business_client
+
+  validates_presence_of :fullname
 end
