afdee0d7ea63c58090ec5ea636055e60d98a754a
paranoiase Kang <paranoiase@gmail.com>
2012-04-12 10:52:48 +0900

    applying decent exposure to taxbill itmes

--- app/controllers/taxbill_items_controller.rb
+++ app/controllers/taxbill_items_controller.rb
@@ -1,34 +1,20 @@
 class TaxbillItemsController < ApplicationController
-  before_filter :find_taxbill
-
-  def new
-    @item = @taxbill.items.new
-  end
+  expose(:taxbill)
+  expose(:taxbill_items) { taxbill.items }
+  expose(:taxbill_item)
 
   def create
-    @item = @taxbill.items.build(params[:taxbill_item])
-    @item.save!
-    redirect_to @taxbill
-  end
-
-  def edit
-    @item = @taxbill.items.find(params[:id])
+    taxbill_item.save!
+    redirect_to taxbill
   end
 
   def update
-    @item = @taxbill.items.find(params[:id])
-    @item.update_attributes!(params[:taxbill_item])
-    redirect_to @taxbill
+    taxbill_item.save!
+    redirect_to taxbill
   end
 
   def destroy
-    @item = @taxbill.items.find(params[:id])
-    @item.destroy
-    redirect_to @taxbill
-  end
-
-  private
-  def find_taxbill
-    @taxbill = Taxbill.find(params[:taxbill_id])
+    taxbill_item.destroy
+    redirect_to taxbill
   end
 end
