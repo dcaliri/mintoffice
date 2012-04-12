64328e0bded315d476db4288e9b69a705ed8b11d
paranoiase Kang <paranoiase@gmail.com>
2012-04-12 10:32:42 +0900

    applied CRUD of taxbill items

--- app/controllers/taxbill_items_controller.rb
+++ app/controllers/taxbill_items_controller.rb
@@ -5,6 +5,28 @@ class TaxbillItemsController < ApplicationController
     @item = @taxbill.items.new
   end
 
+  def create
+    @item = @taxbill.items.build(params[:taxbill_item])
+    @item.save!
+    redirect_to @taxbill
+  end
+
+  def edit
+    @item = @taxbill.items.find(params[:id])
+  end
+
+  def update
+    @item = @taxbill.items.find(params[:id])
+    @item.update_attributes!(params[:taxbill_item])
+    redirect_to @taxbill
+  end
+
+  def destroy
+    @item = @taxbill.items.find(params[:id])
+    @item.destroy
+    redirect_to @taxbill
+  end
+
   private
   def find_taxbill
     @taxbill = Taxbill.find(params[:taxbill_id])
--- config/routes.rb
+++ config/routes.rb
@@ -24,7 +24,7 @@ Mintoffice::Application.routes.draw do
     resources :taxmen, :except => :index
   end
   resources :taxbills do
-    resources :items, controller: 'taxbill_items'
+    resources :taxbill_items, :path => "items"
   end
 
   # The priority is based upon order of creation: first created -> highest priority.
@@ -73,70 +73,70 @@ end
 
 # ActionController::Routing::Routes.draw do |map|
 #   map.resources :creditcards
-# 
+#
 #   map.resources :documents
-# 
+#
 #   map.resources :projects
-# 
+#
 #   map.resources :pettycashes
-# 
+#
 #   map.resources :permissions
-# 
+#
 #   map.resources :cardbills
-# 
+#
 #   map.connect '/hrinfos/retire/:id', :controller => "hrinfos", :action => "retire", :conditions => {:method => :get}
 #   map.connect '/hrinfos/retire/:id', :controller => "hrinfos", :action => "retire_save", :conditions => {:method => :post}
 #   map.resources :hrinfos
-# 
+#
 #   map.resources :attachments
-# 
+#
 #   map.connect '/users/changepw/:user_id', :controller => 'users', :action => 'changepw'
 #   map.connect '/users/login', :controller => 'users', :action => 'login', :conditions => { :method => :get}
 #   map.connect '/users/logout', :controller => 'users', :action => 'logout', :conditions => { :method => :get}
 #   map.connect '/users/my', :controller => "users", :action => "my", :conditions => {:method => :get}
 #   map.resources :users
-#   
+#
 #   map.resources :required_tags
-#   
+#
 #   map.resources :namecards
-# 
+#
 #   # The priority is based upon order of creation: first created -> highest priority.
-# 
+#
 #   # Sample of regular route:
 #   #   map.connect 'products/:id', :controller => 'catalog', :action => 'view'
 #   # Keep in mind you can assign values other than :controller and :action
-# 
+#
 #   # Sample of named route:
 #   #   map.purchas3e 'products/:id/purchase', :controller => 'catalog', :action => 'purchase'
 #   # This route can be invoked with purchase_url(:id => product.id)
-# 
+#
 #   # Sample resource route (maps HTTP verbs to controller actions automatically):
 #   #   map.resources :products
-# 
+#
 #   # Sample resource route with options:
 #   #   map.resources :products, :member => { :short => :get, :toggle => :post }, :collection => { :sold => :get }
-# 
+#
 #   # Sample resource route with sub-resources:
 #   #   map.resources :products, :has_many => [ :comments, :sales ], :has_one => :seller
-#   
+#
 #   # Sample resource route with more complex sub-resources
 #   #   map.resources :products do |products|
 #   #     products.resources :comments
 #   #     products.resources :sales, :collection => { :recent => :get }
 #   #   end
-# 
+#
 #   # Sample resource route within a namespace:
 #   #   map.namespace :admin do |admin|
 #   #     # Directs /admin/products/* to Admin::ProductsController (app/controllers/admin/products_controller.rb)
 #   #     admin.resources :products
 #   #   end
-# 
+#
 #   # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
 #   # map.root :controller => "welcome"
 #   map.root :controller => "main"
-# 
+#
 #   # See how all your routes lay out with "rake routes"
-# 
+#
 #   # Install the default routes as the lowest priority.
 #   # Note: These default routes make all actions in every controller accessible via GET requests. You should
 #   # consider removing or commenting them out if you're using named routes and resources.
