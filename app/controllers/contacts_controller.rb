468ba21441c68d35d03640da21c7bfe56ceb9655
paranoiase Kang <paranoiase@gmail.com>
2012-04-26 13:20:50 +0900

    apply attachment(like namecard)

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -2,14 +2,25 @@ class ContactsController < ApplicationController
   expose(:contacts) { Contact.all }
   expose(:contact)
 
+  def show
+    @attachments = Attachment.for_me(contact, "seq ASC")
+    session[:attachments] = [] if session[:attachments].nil?
+    @attachments.each { |at| session[:attachments] << at.id }
+  end
+
+  def edit
+    @attachments = Attachment.for_me(contact, "seq ASC")
+  end
 
   def create
     contact.save!
+    Attachment.save_for(contact, @user, params[:attachment])
     redirect_to contact
   end
 
   def update
     contact.save!
+    Attachment.save_for(contact, @user, params[:attachment])
     redirect_to contact
   end
 
