f3385b1d784f978658491201a2c124f1eef19b8c
paranoiase Kang <paranoiase@gmail.com>
2012-04-26 12:11:42 +0900

    remove blank select boxk (#8159)

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,12 +1,15 @@
 # encoding: UTF-8
 
 class Contact < ActiveRecord::Base
+
+  REJECT_IF_EMPTY = proc { |attrs| attrs.all? { |k, v| k != "target" ? v.blank? : true  } }
+
   has_many :addresses, class_name: 'ContactAddress', :dependent => :destroy
-  accepts_nested_attributes_for :addresses, :allow_destroy => :true, :reject_if => proc { |attrs| attrs.all? { |k, v| v.blank? } }
+  accepts_nested_attributes_for :addresses, :allow_destroy => :true, :reject_if => REJECT_IF_EMPTY
 
   has_many :emails, class_name: 'ContactEmail', :dependent => :destroy
-  accepts_nested_attributes_for :emails, :allow_destroy => :true, :reject_if => proc { |attrs| attrs.all? { |k, v| v.blank? } }
+  accepts_nested_attributes_for :emails, :allow_destroy => :true, :reject_if => REJECT_IF_EMPTY
 
   has_many :phone_numbers, class_name: 'ContactPhoneNumber', :dependent => :destroy
-  accepts_nested_attributes_for :phone_numbers, :allow_destroy => :true, :reject_if => proc { |attrs| attrs.all? { |k, v| v.blank? } }
+  accepts_nested_attributes_for :phone_numbers, :allow_destroy => :true, :reject_if => REJECT_IF_EMPTY
 end
