10c710452d38b6845ef5a2706ceee389d3f2b6d4
paranoiase Kang <paranoiase@gmail.com>
2012-04-27 11:37:15 +0900

    fixed. closes #8170

--- app/models/card_approved_source.rb
+++ app/models/card_approved_source.rb
@@ -39,6 +39,22 @@ class CardApprovedSource < ActiveRecord::Base
     collection.first unless collection.empty?
   end
 
+  def used_at_to_s
+    attribute = read_attribute(:used_at)
+    attribute.blank? ? "" : attribute.strftime("%Y.%m.%d %H:%M")
+  end
+
+  def canceled_at_to_s
+    attribute = read_attribute(:canceled_at)
+    attribute.blank? ? "" : attribute.strftime("%Y %m.%d")
+  end
+
+
+  def will_be_paied_at_to_s
+    attribute = read_attribute(:will_be_paied_at)
+    attribute.blank? ? "" : attribute.strftime("%Y %m.%d")
+  end
+
   class << self
     def open_and_parse_stylesheet(card, upload)
       @card = card
