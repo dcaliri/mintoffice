8ff1507af53c4423f5af1ccc9025f563d3d7fcc1
paranoiase Kang <paranoiase@gmail.com>
2012-04-30 11:13:37 +0900

    modify search boundary. closes #8225

--- app/models/contact_email.rb
+++ app/models/contact_email.rb
@@ -1,4 +1,8 @@
 class ContactEmail < ActiveRecord::Base
   belongs_to :contact
   has_and_belongs_to_many :tags, :class_name => 'ContactEmailTag'
+
+  def self.search(query)
+    where("email like ?", query)
+  end
 end
