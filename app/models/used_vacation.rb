24b548530bdc0ad12a2f33f4e9c7d7503609e93b
paranoiase Kang <paranoiase@gmail.com>
2012-05-08 15:55:23 +0900

    complete. closes #8305

--- app/models/used_vacation.rb
+++ app/models/used_vacation.rb
@@ -1,9 +1,17 @@
+#encoding: UTF-8
+
 class UsedVacation < ActiveRecord::Base
   belongs_to :vacation
 
   include Historiable
   def history_parent
     vacation
+  end
+  def history_info
+    {
+      from: proc { |used, v| "[사용 연차]#{v}" },
+      to: proc { |used, v| "[사용 연차]#{v}" }
+    }
   end
 
   def self.total
