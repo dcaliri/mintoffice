4ded876f0a6879814ebc065d6861e8eb77bca3b8
paranoiase Kang <paranoiase@gmail.com>
2012-05-09 15:32:22 +0900

    shouldn't destroy attachment when parent is removed

--- lib/attachmentable.rb
+++ lib/attachmentable.rb
@@ -2,7 +2,7 @@ module Attachmentable
   extend ActiveSupport::Concern
 
   included do
-    has_many :attachments, :as => :owner, :dependent => :destroy
+    has_many :attachments, :as => :owner
     accepts_nested_attributes_for :attachments
   end
 end
