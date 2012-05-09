9bf6210d359bb15ba7598816f87b725b7fe0c759
paranoiase Kang <paranoiase@gmail.com>
2012-05-09 11:46:57 +0900

    ordering. destroy attachments when parents was removed

--- lib/attachmentable.rb
+++ lib/attachmentable.rb
@@ -2,7 +2,7 @@ module Attachmentable
   extend ActiveSupport::Concern
 
   included do
-    has_many :attachments, :as => :owner
+    has_many :attachments, :as => :owner, :dependent => :destroy
     accepts_nested_attributes_for :attachments
   end
 end
