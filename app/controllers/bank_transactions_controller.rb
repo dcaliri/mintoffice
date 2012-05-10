a1acaaa90d7f3cbed50e9ebff07b0d793b7d616b
paranoiase Kang <paranoiase@gmail.com>
2012-05-10 15:23:36 +0900

    refactor bank transfers. closes #8463

--- app/controllers/bank_transactions_controller.rb
+++ app/controllers/bank_transactions_controller.rb
@@ -31,26 +31,11 @@ class BankTransactionsController < ApplicationController
   def update
     bank_transaction.bank_account = bank_account
     bank_transaction.save!
-    redirect_to @bank_transaction
+    redirect_to bank_transaction
   end
 
   def destroy
     bank_transaction.destroy
     redirect_to :bank_transactions
   end
-
-  private
-  def current_year
-    params[:at] = Time.zone.now.year unless params[:at]
-    Time.zone.parse("#{params[:at]}-01-01 00:00:00")
-  end
-  helper_method :current_year
-
-  def oldest_year
-    in_ = @in.oldest_at
-    out_ = @out.oldest_at
-
-    [in_, out_].min.year
-  end
-  helper_method :oldest_year
 end
