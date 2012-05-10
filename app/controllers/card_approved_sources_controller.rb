dad62a666ddab2aa44ea88a2bb013e8d14995574
paranoiase Kang <paranoiase@gmail.com>
2012-05-10 14:56:18 +0900

    refactor card approved sources

--- app/controllers/card_approved_sources_controller.rb
+++ app/controllers/card_approved_sources_controller.rb
@@ -1,50 +1,26 @@
 class CardApprovedSourcesController < ApplicationController
-  before_filter :find_creditcard
-  def find_creditcard
-    @creditcard = Creditcard.find(params[:creditcard_id]) unless params[:creditcard_id].blank?
-  end
+  expose(:creditcard) { Creditcard.find(params[:creditcard_id]) unless params[:creditcard_id].blank? }
+  expose(:card_approved_source)
 
   def index
-    source = @creditcard.nil? ? CardApprovedSource : @creditcard.card_approved_sources
-    @card_approved_sources = source.latest.page(params[:page])
-  end
-
-  def show
-    @card_approved_source = CardApprovedSource.find(params[:id])
-  end
-
-  def preview
-    @card_approved_sources = CardApprovedSource.preview_stylesheet(creditcard, params[:upload])
-  end
-
-  def upload
-    @card_approved_sources.create_with_stylesheet(creditcard, params[:upload])
-    redirect_to :card_approved_sources
-  end
-
-  def new
-    @card_approved_source = CardApprovedSource.new
+    approvd_source = creditcard.nil? ? CardApprovedSource : creditcard.card_approved_sources
+    @card_approved_sources = approvd_source.latest.page(params[:page])
   end
 
   def create
-    @card_approved_source = @creditcard.card_approved_sources.build(params[:card_approved_source])
-    @card_approved_source.save!
-    redirect_to @card_approved_source
-  end
-
-  def edit
-    @card_approved_source = CardApprovedSource.find(params[:id])
+    card_approved_source = creditcard.card_approved_sources.build(params[:card_approved_source])
+    card_approved_source.save!
+    redirect_to card_approved_source
   end
 
   def update
-    @card_approved_source = CardApprovedSource.find(params[:id])
-    @card_approved_source.save!
-    redirect_to @card_approved_source
+    card_approved_source.creditcard = creditcard
+    card_approved_source.save!
+    redirect_to card_approved_source
   end
 
   def destroy
-    @card_approved_source = CardApprovedSource.find(params[:id])
-    @card_approved_source.destroy
+    card_approved_source.destroy
     redirect_to :card_approved_sources
   end
 end
