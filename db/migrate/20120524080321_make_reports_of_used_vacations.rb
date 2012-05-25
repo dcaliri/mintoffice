1f96850936c260ea0edb487fc5a87f269c1afd97
paranoiase Kang <paranoiase@gmail.com>
2012-05-25 17:56:14 +0900

    fix. closes #8636

--- db/migrate/20120524080321_make_reports_of_used_vacations.rb
+++ db/migrate/20120524080321_make_reports_of_used_vacations.rb
@@ -5,23 +5,28 @@ class MakeReportsOfUsedVacations < ActiveRecord::Migration
 
   def up
     user = Group.where(name: "admin").first.users.first
-    UsedVacation.find_each do |target|
-      report = Report.new
-      report.target_type = "UsedVacation"
-      report.target_id = target.id
-      report.status = :not_reported
-      report.save!
 
-      reporter = ReportPerson.new(report_id: report.id, user_id: user.id, permission_type: :write, owner: true)
-      reporter.save!
+    execute <<-SQL
+      INSERT INTO reports (status, target_id, target_type)
+      SELECT 'not_reported', id, 'UsedVacation'
+      FROM used_vacations;
+    SQL
 
-      target.save!
-    end
+    execute <<-SQL
+      INSERT INTO report_people (user_id, report_id, permission_type, owner, prev_id, created_at, updated_at)
+      SELECT "#{user.id}", id, "write", "t", "", date('now'), date('now')
+      FROM reports
+      WHERE reports.target_type = 'UsedVacation';
+    SQL
   end
 
   def down
-    Report.where(target_type: "UsedVacation").find_each do |report|
-      report.destroy
-    end
+    execute <<-SQL
+      DELETE FROM report_people WHERE report_id IN (SELECT id FROM reports WHERE target_type = "UsedVacation");
+    SQL
+
+    execute <<-SQL
+      DELETE FROM reports WHERE target_type = "UsedVacation";
+    SQL
   end
 end
