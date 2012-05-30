7aab5adedf5d87b5371a2854a1f259f4d1060f20
paranoiase Kang <paranoiase@gmail.com>
2012-05-30 15:22:36 +0900

    fix migration bug

--- db/migrate/20120529053831_add_company_id_to_tag.rb
+++ db/migrate/20120529053831_add_company_id_to_tag.rb
@@ -2,7 +2,7 @@ class AddCompanyIdToTag < ActiveRecord::Migration
   def change
     add_column :tags, :company_id, :integer
 
-    company = Company.find_by_name("minttech")
+    company = Company.find_or_create_by_name("minttech")
     Tag.update_all(company_id: company.id)
   end
 end
