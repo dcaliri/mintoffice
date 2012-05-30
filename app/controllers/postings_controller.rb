be77ca6725783bb55d8d62f62362bc41188f6933
paranoiase Kang <paranoiase@gmail.com>
2012-05-30 17:38:49 +0900

    fix. closes #8823

--- app/controllers/postings_controller.rb
+++ app/controllers/postings_controller.rb
@@ -13,11 +13,28 @@ class PostingsController < ApplicationController
     end
   end
 
+  def edit
+    @posting = Posting.find(params[:id])
+  end
+
   def create
     posting.save!
     redirect_to posting
   rescue ActiveRecord::RecordInvalid
     @posting = posting
     render 'new'
+  end
+
+  def update
+    posting.save!
+    redirect_to posting
+  rescue ActiveRecord::RecordInvalid
+    @posting = posting
+    render 'new'
+  end
+
+  def destroy
+    posting.destroy
+    redirect_to :postings
   end
 end
