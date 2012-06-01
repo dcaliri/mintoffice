a7fedbbe73ba5963b758f9e9a2e395bb62f40c2c
paranoiase Kang <paranoiase@gmail.com>
2012-06-01 18:23:05 +0900

    pass documents controller test

--- test/functional/documents_controller_test.rb
+++ test/functional/documents_controller_test.rb
@@ -2,26 +2,39 @@ require 'test_helper'
 
 class DocumentsControllerTest < ActionController::TestCase
   def setup
-    User.create(id: 37, name: "admin", password: "1234")
-    session[:user_id] = 37
+    User.destroy_all
+    @user = User.create!(name: "admin", password: "1234", password_confirmation: "1234")
+    @user.permission.create!(name: 'documents')
+
+    Company.destroy_all
+    @company = Company.create!(name: "minttech")
+
+    session[:user_id] = @user.id
+    session[:company_id] = @company.id
+
+    User.current_user = @user
+    Company.current_company = @company
+
+    Document.destroy_all
+    @document = @company.documents.create!(title: 'HAHA')
   end
 
-  test "should get index" do
+  test "should index document list" do
     get :index
     assert_response :success
   end
 
-  test "should get new" do
+  test "should show new document form" do
     get :new
     assert_response :success
   end
 
-  # test "should show user" do
-  #   get :show, :id => 37
-  # end
-  #
-  # test "should get edit" do
-  #   get :edit, :id => 37
-  #   assert_response :success
-  # end
+  test "should show document" do
+    get :show, :id => @document.id
+  end
+
+  test "should edit document" do
+    get :edit, :id => @document.id
+    assert_response :success
+  end
 end
