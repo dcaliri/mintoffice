c61e6bb43a42bc9244265253c0a957b879ef3763
paranoiase Kang <paranoiase@gmail.com>
2012-06-04 18:21:03 +0900

    add company information to employment proof #8904

--- app/models/hrinfo.rb
+++ app/models/hrinfo.rb
@@ -91,6 +91,10 @@ class Hrinfo < ActiveRecord::Base
     save!
   end
 
+  def company
+    Company.first
+  end
+
   def generate_employment_proof
     filename = "#{Rails.root}/tmp/#{fullname}_employment_proof.pdf"
     template = "#{Rails.root}/app/assets/images/employment_proof_tempate.pdf"
@@ -102,8 +106,12 @@ class Hrinfo < ActiveRecord::Base
       pdf.draw_text fullname, :at => [142, 685]
       pdf.draw_text juminno, :at => [142, 661]
       pdf.draw_text address, :at => [142, 637]
-      pdf.draw_text phone_number, :at => [333, 589]
       pdf.draw_text position, :at => [333, 526]
+
+      pdf.draw_text company.registration_number, :at => [333, 613]
+      pdf.draw_text company.owner_name, :at => [142, 589]
+      pdf.draw_text company.address, :at => [142, 558]
+      pdf.draw_text company.phone_number, :at => [333, 589]
     end
 
     filename
