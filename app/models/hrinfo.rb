b088d9b63ba651006a0cc1702afcf5d3600013e8
paranoiase Kang <paranoiase@gmail.com>
2012-06-05 14:43:16 +0900

    strip address info #8904

--- app/models/hrinfo.rb
+++ app/models/hrinfo.rb
@@ -113,7 +113,7 @@ class Hrinfo < ActiveRecord::Base
 
       pdf.draw_text fullname, :at => [142, 685]
       pdf.draw_text juminno, :at => [142, 661]
-      pdf.draw_text address, :at => [142, 637]
+      pdf.draw_text address.strip, :at => [142, 637]
       pdf.draw_text department, :at => [142, 526]
       pdf.draw_text position, :at => [333, 526]
 
