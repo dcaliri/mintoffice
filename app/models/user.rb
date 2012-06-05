8163f980b520cc2eb9baaf2bd8aa65cb89879749
Sooyong Wang <wangsy@wangsy.com>
2012-06-05 13:21:39 +0900

    fix changepw

--- app/models/user.rb
+++ app/models/user.rb
@@ -22,7 +22,7 @@ class User < ActiveRecord::Base
 
   validates_presence_of :name
   validates_uniqueness_of :name
-  validates_uniqueness_of :gmail_account, :if => "! gmail_account.empty?"
+  validates_uniqueness_of :gmail_account, :unless => "gmail_account.nil? || gmail_account.empty?"
   attr_accessor :password_confirmation
   validates_confirmation_of :password, :if => Proc.new{|user| user.provider.blank? and user.uid.blank?}
   validate :password_non_blank, :if => Proc.new{|user| user.provider.blank? and user.uid.blank?}
