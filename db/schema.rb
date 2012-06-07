b2301fd8f6baacc7cbb9373c4ff2bd630c403be9
Sooyong Wang <wangsy@wangsy.com>
2012-06-07 22:04:14 +0900

    add gemfile, schema

--- db/schema.rb
+++ db/schema.rb
@@ -26,8 +26,8 @@ ActiveRecord::Schema.define(:version => 20120605050447) do
     t.string   "title"
     t.string   "comments"
     t.string   "filepath"
-    t.datetime "created_at",                        :null => false
-    t.datetime "updated_at",                        :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.integer  "user_id",           :default => 0,  :null => false
     t.string   "contenttype",       :default => "", :null => false
     t.string   "owner_type"
@@ -155,8 +155,8 @@ ActiveRecord::Schema.define(:version => 20120605050447) do
     t.string   "storename"
     t.string   "storeaddr"
     t.string   "approveno"
-    t.datetime "created_at",                                   :null => false
-    t.datetime "updated_at",                                   :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.integer  "attachment_id"
     t.integer  "creditcard_id"
     t.boolean  "before_report"
@@ -297,8 +297,8 @@ ActiveRecord::Schema.define(:version => 20120605050447) do
     t.string   "nickname"
     t.string   "issuer"
     t.string   "cardholder"
-    t.datetime "created_at",  :null => false
-    t.datetime "updated_at",  :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.string   "short_name"
   end
 
@@ -323,15 +323,15 @@ ActiveRecord::Schema.define(:version => 20120605050447) do
   create_table "document_owners", :force => true do |t|
     t.integer  "document_id"
     t.integer  "user_id"
-    t.datetime "created_at",  :null => false
-    t.datetime "updated_at",  :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "documents", :force => true do |t|
     t.string   "title"
     t.integer  "project_id"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.integer  "company_id"
   end
 
@@ -356,8 +356,8 @@ ActiveRecord::Schema.define(:version => 20120605050447) do
 
   create_table "groups", :force => true do |t|
     t.string   "name"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "groups_users", :id => false, :force => true do |t|
@@ -376,16 +376,16 @@ ActiveRecord::Schema.define(:version => 20120605050447) do
     t.integer  "hrinfo_id"
     t.string   "change"
     t.integer  "user_id"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "hrinfos", :force => true do |t|
     t.string   "firstname"
     t.string   "lastname"
     t.integer  "picture_id"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.date     "joined_on"
     t.date     "retired_on"
     t.integer  "user_id"
@@ -413,8 +413,8 @@ ActiveRecord::Schema.define(:version => 20120605050447) do
     t.string   "phone"
     t.string   "homepage"
     t.string   "email"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "payments", :force => true do |t|
@@ -453,8 +453,8 @@ ActiveRecord::Schema.define(:version => 20120605050447) do
 
   create_table "permissions", :force => true do |t|
     t.string   "name"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "permissions_users", :id => false, :force => true do |t|
@@ -470,8 +470,8 @@ ActiveRecord::Schema.define(:version => 20120605050447) do
     t.decimal  "outmoney",      :precision => 10, :scale => 0, :default => 0, :null => false
     t.text     "description"
     t.integer  "attachment_id"
-    t.datetime "created_at",                                                  :null => false
-    t.datetime "updated_at",                                                  :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "posting_items", :force => true do |t|
@@ -511,8 +511,8 @@ ActiveRecord::Schema.define(:version => 20120605050447) do
     t.date     "ending_on"
     t.date     "ended_on"
     t.decimal  "revenue",    :precision => 20, :scale => 0
-    t.datetime "created_at",                                :null => false
-    t.datetime "updated_at",                                :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.integer  "company_id"
   end
 
@@ -543,8 +543,8 @@ ActiveRecord::Schema.define(:version => 20120605050447) do
   create_table "required_tags", :force => true do |t|
     t.string   "modelname"
     t.integer  "tag_id"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "taggings", :force => true do |t|
@@ -558,8 +558,8 @@ ActiveRecord::Schema.define(:version => 20120605050447) do
 
   create_table "tags", :force => true do |t|
     t.string   "name"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.integer  "company_id"
   end
 
@@ -606,8 +606,8 @@ ActiveRecord::Schema.define(:version => 20120605050447) do
     t.string   "name"
     t.string   "hashed_password"
     t.string   "salt"
-    t.datetime "created_at",      :null => false
-    t.datetime "updated_at",      :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.string   "provider"
     t.string   "uid"
     t.string   "gmail_account"
