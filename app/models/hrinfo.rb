77d1efcf12d898c954e23784f760d9ed80f4fab6
paranoiase Kang <paranoiase@gmail.com>
2012-06-08 13:54:55 +0900

    move hash_key to bottom. closes #9052

--- app/models/hrinfo.rb
+++ app/models/hrinfo.rb
@@ -153,8 +153,7 @@ class Hrinfo < ActiveRecord::Base
       pdf.draw_text company.name, :at => [250, 255]
       pdf.draw_text company.owner_name, :at => [250, 225]
 
-      pdf.move_cursor_to 170
-      pdf.text hash_key, align: :center
+      pdf.draw_text "*  발급확인코드: #{hash_key}", :at => [14, 45], :size => 10
     end
 
     employment_proof_hash << hash_key
