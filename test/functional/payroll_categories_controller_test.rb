31ab24b99010d2071494241ff203ef0fc12fa20b
paranoiase Kang <paranoiase@gmail.com>
2012-06-08 16:51:41 +0900

    add holidays, payrolls and dayworkers test #8902

---
+++ test/functional/payroll_categories_controller_test.rb
@@ -0,0 +1,34 @@
+require 'test_helper'
+
+class PayrollCategoriesControllerTest < ActionController::TestCase
+  fixtures :users, :hrinfos, :payrolls, :payroll_categories
+
+  def setup
+    current_user.permission.create!(name: 'payroll_categories')
+  end
+
+  test "should see index page" do
+    get :index
+    assert_response :success
+  end
+
+  test "should see new page" do
+    get :new
+    assert_response :success
+  end
+
+  test "should see show page" do
+    get :show, :id => current_payroll_category.id
+    assert_response :success
+  end
+
+  test "should see edit page" do
+    get :edit, :id => current_payroll_category.id
+    assert_response :success
+  end
+
+  private
+  def current_payroll_category
+    @payroll_category ||= payroll_categories(:fixture)
+  end
+end
---
+++ test/functional/payrolls_controller_test.rb
@@ -0,0 +1,34 @@
+require 'test_helper'
+
+class PayrollsControllerTest < ActionController::TestCase
+  fixtures :users, :hrinfos, :payrolls
+
+  def setup
+    current_user.permission.create!(name: 'documents')
+  end
+
+  test "should see index page" do
+    get :index
+    assert_response :success
+  end
+
+  test "should see new page" do
+    get :new
+    assert_response :success
+  end
+
+  test "should see show page" do
+    get :show, :id => current_payroll.id
+    assert_response :success
+  end
+
+  test "should see edit page" do
+    get :edit, :id => current_payroll.id
+    assert_response :success
+  end
+
+  private
+  def current_payroll
+    @payroll ||= payrolls(:fixture)
+  end
+end
