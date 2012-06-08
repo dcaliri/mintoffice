17fdb9c710d67ff0c69ead9c6f9208020a5305c4
paranoiase Kang <paranoiase@gmail.com>
2012-06-08 16:19:42 +0900

    add payment and pettycash test #8902

---
+++ test/functional/pettycashes_controller_test.rb
@@ -0,0 +1,34 @@
+require 'test_helper'
+
+class PettycashesControllerTest < ActionController::TestCase
+  fixtures :pettycashes
+
+  def setup
+    current_user.permission.create!(name: 'pettycashes')
+  end
+
+  test "should see index page" do
+    get :index
+    assert_response :success
+  end
+
+  test "should see new page" do
+    get :new
+    assert_response :success
+  end
+
+  test "should see show page" do
+    get :show, :id => current_pettycash.id
+    assert_response :success
+  end
+
+  test "should see edit page" do
+    get :edit, :id => current_pettycash.id
+    assert_response :success
+  end
+
+  private
+  def current_pettycash
+    @pettycash ||= pettycashes(:fixture)
+  end
+end
