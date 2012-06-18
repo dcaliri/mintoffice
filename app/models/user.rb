c65ef818eac2cfa0affe97e08fc612e37c39b561
paranoiase Kang <paranoiase@gmail.com>
2012-06-18 14:38:03 +0900

    should create google apps account #9081

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 require 'digest/sha1'
+# require 'nokogiri'
+# require 'google_apps'
 
 class User < ActiveRecord::Base
   has_many :attachment
@@ -151,6 +153,31 @@ class User < ActiveRecord::Base
 
   def has_payment_info
     not payments.empty?
+  end
+
+  def google_apps_configure
+    OpenStruct.new(YAML.load_file("config/google_apps.yml"))
+  rescue => e
+    raise Errno::ENOENT, "no google app configure file. please create config/google_apps.yml"
+  end
+
+  def google_app_account
+    config = google_apps_configure
+    "#{name}@#{config.domain}"
+  end
+
+  def create_google_app_account
+    config = google_apps_configure
+    transporter = GoogleApps::Transport.new config.domain
+    transporter.authenticate config.username, config.password
+
+    # Creating a User
+    user = GoogleApps::Atom::User.new
+    user.new_user name, hrinfo.firstname, hrinfo.lastname, config.default_password.to_s, 2048
+    transporter.new_user user
+
+    doc = Nokogiri::XML(transporter.response.body)
+    !doc.xpath('//AppsForYourDomainErrors/error').first['errorCode'] rescue true
   end
 
 private
