7fa72e1c61652886d64921d700f0a15f56e0d8a1
paranoiase Kang <paranoiase@gmail.com>
2012-06-28 18:33:02 +0900

    modify mailer configure option. closes #9277

--- app/mailer/report_mailer.rb
+++ app/mailer/report_mailer.rb
@@ -1,6 +1,6 @@
 class ReportMailer
   def self.configure
-    @config ||= OpenStruct.new(YAML.load_file('config/mailer.yml'))
+    @config ||= Mintoffice::Application.config.mailer
   end
 
   def self.report(target, from, to, subject, message)
@@ -8,17 +8,7 @@ class ReportMailer
       :from => from.notify_email,
       :to => to.notify_email,
       :subject => subject,
-      :body => message,
-      :via => :smtp,
-      :via_options => {
-        :address              => configure.address,
-        :port                 => configure.port,
-        :enable_starttls_auto => true,
-        :user_name            => configure.username,
-        :password             => configure.password,
-        :authentication       => :plain,
-        :domain               => configure.domain
-      }
-    })
+      :body => message
+    }.merge(configure))
   end
 end
