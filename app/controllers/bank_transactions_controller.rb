b91e9b9b2e0c77d3cfd47708e8c9881232677628
paranoiase Kang <paranoiase@gmail.com>
2012-06-29 15:45:01 +0900

    complete. closes #9351

--- app/controllers/bank_transactions_controller.rb
+++ app/controllers/bank_transactions_controller.rb
@@ -6,6 +6,7 @@ class BankTransactionsController < ApplicationController
   expose(:bank_transfer) { BankTransfer.find(params[:from]) if params[:from] }
 
   def index
+    params[:bank_account_id] ||= BankAccount.first.id
     transactions = bank_account ? bank_account.bank_transactions : BankTransaction
     @bank_transactions = transactions.latest.page(params[:page])
   end
