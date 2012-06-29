1708deac883fe429d0f40dabb071bc3cce3a5853
paranoiase Kang <paranoiase@gmail.com>
2012-06-29 17:20:23 +0900

    verify bank transaction. #9352

--- app/controllers/bank_transactions_controller.rb
+++ app/controllers/bank_transactions_controller.rb
@@ -22,7 +22,7 @@ class BankTransactionsController < ApplicationController
 
   def preview
     bank_account = BankAccount.find(params[:bank_account])
-    @transactions = BankTransaction.preview_stylesheet(bank_account, bank_account.name_, params[:upload])
+    @bank_transactions = BankTransaction.preview_stylesheet(bank_account, bank_account.name_, params[:upload])
   rescue => error
     redirect_to [:excel, :bank_transactions], alert: error.message
   end
