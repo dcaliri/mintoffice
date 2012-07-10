852d66560cd27ddb303b60eed213faed7849e6d6
paranoiase Kang <paranoiase@gmail.com>
2012-07-10 13:34:47 +0900

    refactor enrollment controller

--- app/controllers/section_enrollment/enrollments_controller.rb
+++ app/controllers/section_enrollment/enrollments_controller.rb
@@ -2,20 +2,14 @@
 
 class SectionEnrollment::EnrollmentsController < ApplicationController
   skip_before_filter :authorize
+  before_filter :find_enrollment, :except => :delete_attachment
   before_filter :find_apply_admin
 
-  def dashboard
-    @enrollment = current_user.enrollment
-  end
-
   def edit
-    @this_user = current_user
-    @enrollment = current_user.enrollment
     @child_contact = @enrollment.contact || @enrollment.build_contact
   end
 
   def update
-    @enrollment = current_user.enrollment
     if @enrollment.update_attributes(params[:enrollment])
       redirect_to [:dashboard, :enrollments], notice: '입사지원서가 저장되었습니다.'
     else
@@ -25,12 +19,10 @@ class SectionEnrollment::EnrollmentsController < ApplicationController
   end
 
   def attach_item
-    @enrollment = current_user.enrollment
     @item = @enrollment.items.find_by_name(params[:name])
   end
 
   def attach
-    @enrollment = current_user.enrollment
     item = @enrollment.find_or_create_item_by_name(params[:name])
     @attachment = Attachment.save_for(item, current_user, uploaded_file: params[:picture])
     redirect_to :back
@@ -40,6 +32,11 @@ class SectionEnrollment::EnrollmentsController < ApplicationController
     attachment = Attachment.where(user_id: current_user.id, id: params[:attachment_id]).last
     attachment.destroy
     redirect_to :back
+  end
+
+  private
+  def find_enrollment
+    @enrollment = current_user.enrollment
   end
 
   def find_apply_admin
