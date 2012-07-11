749130672b6383fcbffb0d51862e6310d152b403
paranoiase Kang <paranoiase@gmail.com>
2012-07-11 14:01:51 +0900

    fix boxcar error

--- lib/boxcar.rb
+++ lib/boxcar.rb
@@ -6,17 +6,23 @@ class Boxcar
     
     uri = URI('http://boxcar.io/devices/providers/i7pThv4s27chmnqFz2FJ/notifications/subscribe')
     res = Net::HTTP.post_form(uri, 'email' => email)
+  rescue Errno::ECONNREFUSED => e
+    Rails.logger.info "Boxcar Error = #{e.message}"
   end
   
   def self.send_to_boxcar(email,from,msg)
     if Rails.env.development?
       msg = "[test] " + msg
     end
+
     uri = URI('http://boxcar.io/devices/providers/i7pThv4s27chmnqFz2FJ/notifications/')
     Rails.logger.info "Boxcar = #{email}"
+
     res = Net::HTTP.post_form(uri, 'email' => email,
             'notification[from_screen_name]' => from,
-            'notification[message]' => msg);
+            'notification[message]' => msg)
+  rescue Errno::ECONNREFUSED => e
+    Rails.logger.info "Boxcar Error = #{e.message}"
   end
 
   def self.send_to_boxcar_group(groupname, from, msg)
