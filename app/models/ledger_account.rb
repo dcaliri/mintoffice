23501d59bb79b1ad04c7c50d4d0f46fed303df5e
손 어지리 <zzilssun@wangsy.com>
2012-07-13 10:19:53 +0900

    tagging #9650

--- app/models/ledger_account.rb
+++ app/models/ledger_account.rb
@@ -5,6 +5,12 @@ class LedgerAccount < ActiveRecord::Base
   has_many :items, class_name: 'PostingItem'
 
   CATEGORY_TYPE = ["자본", "자산", "부채", "비용", "수익"]
+  # listing
+  # models.ledger_account.fund
+  # models.ledger_account.asset
+  # models.ledger_account.debt
+  # models.ledger_account.expense
+  # models.ledger_account.benefit
   def category
     CATEGORY_TYPE[read_attribute(:category) || 0]
   end
