7629b7c239e454992fc09af6944f5dbd907fa7bf
손 어지리 <zzilssun@wangsy.com>
2012-07-17 17:26:09 +0900

    closes #9650, closes #9654

--- app/models/ledger_account.rb
+++ app/models/ledger_account.rb
@@ -4,13 +4,11 @@ class LedgerAccount < ActiveRecord::Base
   # has_many :posting_items
   has_many :items, class_name: 'PostingItem'
 
-  CATEGORY_TYPE = ["자본", "자산", "부채", "비용", "수익"]
-  # listing
-  # models.ledger_account.fund
-  # models.ledger_account.asset
-  # models.ledger_account.debt
-  # models.ledger_account.expense
-  # models.ledger_account.benefit
+  CATEGORY_TYPE = [I18n.t('models.ledger_account.fund'), 
+                  I18n.t('models.ledger_account.asset'), 
+                  I18n.t('models.ledger_account.debt'), 
+                  I18n.t('models.ledger_account.expense'), 
+                  I18n.t('models.ledger_account.benefit')]
   def category
     CATEGORY_TYPE[read_attribute(:category) || 0]
   end
