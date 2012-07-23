f059d5c492d7423bb7cc13a201e27f6757aa958c
손 어지리 <zzilssun@wangsy.com>
2012-07-23 14:58:31 +0900

    add test #9803

---
+++ test/integration/login_test.rb
@@ -0,0 +1,36 @@
+# encoding: UTF-8
+require 'test_helper'
+
+class LoginTest < ActionDispatch::IntegrationTest
+  test 'show login' do
+    clear_session
+    
+    visit '/'
+
+    fill_in "사용자계정", with: "admin"
+    fill_in "비밀번호", with: "1234"
+
+    click_button "로그인"
+
+    assert(page.has_content?('Mint Office'))
+  end
+
+  test 'login fail' do
+    clear_session
+    
+    visit '/'
+
+    fill_in "사용자계정", with: "test"
+    fill_in "비밀번호", with: "1234"
+
+    click_button "로그인"
+
+    assert(!page.has_content?('Mint Office'))
+  end
+
+  test 'show logout' do
+    click_link "로그아웃"
+    
+    assert(page.has_content?('로그인'))
+  end
+end
