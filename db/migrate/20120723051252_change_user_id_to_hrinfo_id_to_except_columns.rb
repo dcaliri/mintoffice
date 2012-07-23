b1efcc782a846c3e33fdc353043e77ca15243836
paranoiase Kang <paranoiase@gmail.com>
2012-07-23 14:25:46 +0900

    modify migration

--- db/migrate/20120723051252_change_user_id_to_hrinfo_id_to_except_columns.rb
+++ db/migrate/20120723051252_change_user_id_to_hrinfo_id_to_except_columns.rb
@@ -13,15 +13,15 @@ class ChangeUserIdToHrinfoIdToExceptColumns < ActiveRecord::Migration
   end
 
   def down
-    add_column :except_columns, :hrinfo_id, :integer
-    
-    ExceptColumn.find_each do |except_column|
-      hrinfo = Hrinfo.find_by_user_id(except_column.user_id)
+    add_column :except_columns, :user_id, :integer
 
-      except_column.hrinfo_id = hrinfo.id
+    ExceptColumn.find_each do |except_column|
+      hrinfo = Hrinfo.find(except_column.hrinfo_id)
+
+      except_column.user_id = hrinfo.user_id
       except_column.save!
     end  
 
-    remove_column :except_columns, :user_id
+    remove_column :except_columns, :hrinfo_id
   end
 end
