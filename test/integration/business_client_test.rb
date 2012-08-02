2c362aa6ecb8b3ac6caa6d8e8bf44efb32bcacb9
paranoiase Kang <paranoiase@gmail.com>
2012-08-02 16:41:01 +0900

    unremove comments

--- test/integration/business_client_test.rb
+++ test/integration/business_client_test.rb
@@ -6,58 +6,58 @@ class BusinessClientTest < ActionDispatch::IntegrationTest
   fixtures :contacts
   fixtures :taxmen
 
-  # test 'should visit business_client list' do
-  #   visit '/'
-  #   click_link '거래처 관리'
+  test 'should visit business_client list' do
+    visit '/'
+    click_link '거래처 관리'
 
-  #   assert(page.has_content?('거래처 관리'))
-  # end
+    assert(page.has_content?('거래처 관리'))
+  end
 
-  # test 'should create a new business_client' do
-  #   visit '/'
-  #   click_link '거래처 관리'
-  #   click_link '신규 작성'
+  test 'should create a new business_client' do
+    visit '/'
+    click_link '거래처 관리'
+    click_link '신규 작성'
 
-  #   fill_in "거래처명", with: "거래처명 입력 테스트"
-  #   fill_in "사업자 등록번호", with: "123-321-12345"
-  #   fill_in "업종", with: "업종 입력 테스트"
-  #   fill_in "업태", with: "업태 입력 테스트"
-  #   fill_in "주소", with: "주소 입력 테스트"
-  #   fill_in "대표자", with: "대표자 입력 테스트"
+    fill_in "거래처명", with: "거래처명 입력 테스트"
+    fill_in "사업자 등록번호", with: "123-321-12345"
+    fill_in "업종", with: "업종 입력 테스트"
+    fill_in "업태", with: "업태 입력 테스트"
+    fill_in "주소", with: "주소 입력 테스트"
+    fill_in "대표자", with: "대표자 입력 테스트"
 
-  #   click_button '거래처 만들기'
+    click_button '거래처 만들기'
 
-  #   assert(page.has_content?('거래처이(가) 성공적으로 생성되었습니다.'))
-  # end
+    assert(page.has_content?('거래처이(가) 성공적으로 생성되었습니다.'))
+  end
 
-  # test 'should edit business_client' do
-  #   visit '/'
-  #   click_link '거래처 관리'
-  #   find("tr.selectable").click
-  #   click_link '수정하기'
+  test 'should edit business_client' do
+    visit '/'
+    click_link '거래처 관리'
+    find("tr.selectable").click
+    click_link '수정하기'
 
-  #   fill_in "거래처명", with: "거래처명 수정 테스트"
-  #   fill_in "사업자 등록번호", with: "321-123-54321"
-  #   fill_in "업종", with: "업종 수정 테스트"
-  #   fill_in "업태", with: "업태 수정 테스트"
-  #   fill_in "주소", with: "주소 수정 테스트"
-  #   fill_in "대표자", with: "대표자 수정 테스트"
+    fill_in "거래처명", with: "거래처명 수정 테스트"
+    fill_in "사업자 등록번호", with: "321-123-54321"
+    fill_in "업종", with: "업종 수정 테스트"
+    fill_in "업태", with: "업태 수정 테스트"
+    fill_in "주소", with: "주소 수정 테스트"
+    fill_in "대표자", with: "대표자 수정 테스트"
 
-  #   click_button '거래처 수정하기'
+    click_button '거래처 수정하기'
 
-  #   assert(page.has_content?('거래처이(가) 성공적으로 업데이트 되었습니다.'))
-  # end
+    assert(page.has_content?('거래처이(가) 성공적으로 업데이트 되었습니다.'))
+  end
 
-  # test 'should destroy business_client' do
-  #   visit '/'
-  #   click_link '거래처 관리'
-  #   find("tr.selectable").click
+  test 'should destroy business_client' do
+    visit '/'
+    click_link '거래처 관리'
+    find("tr.selectable").click
 
-  #   click_link '삭제하기'
-  #   page.driver.browser.switch_to.alert.accept
+    click_link '삭제하기'
+    page.driver.browser.switch_to.alert.accept
 
-  #   assert(page.has_content?('거래처이(가) 성공적으로 제거 되었습니다.'))
-  # end
+    assert(page.has_content?('거래처이(가) 성공적으로 제거 되었습니다.'))
+  end
 
   test 'should add taxman' do
     visit '/'
