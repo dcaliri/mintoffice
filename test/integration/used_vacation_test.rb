3562846f73e3b6a4b4af71c8d070db83d89b3b32
paranoiase Kang <paranoiase@gmail.com>
2012-08-17 18:08:46 +0900

    fix failed test

--- test/integration/used_vacation_test.rb
+++ test/integration/used_vacation_test.rb
@@ -80,12 +80,14 @@ class UsedVacationTest < ActionDispatch::IntegrationTest
 
   test 'should destroy used_vacation' do
     visit '/'
-    
+
     click_link '연차 정보'
     find("tr.selectable").click
 
+    disable_confirm_box
+
     click_link '삭제하기'
-    
+
     assert(page.has_content?('사용한 연차 내역이 없습니다'))
   end
 end
