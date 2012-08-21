74f600db2a247640b385cb509c7cb06492c14f98
손 어지리 <zzilssun@wangsy.com>
2012-08-21 09:13:28 +0900

    remove bak directory

--- test/무제 폴더/root_test.rb
+++
@@ -1,8 +0,0 @@
-# encoding: UTF-8
-require 'test_helper'
-
-class RootTest < ActionDispatch::IntegrationTest
-  test 'show visit root page' do
-    assert(page.has_content?('Mint Office'))
-  end
-end
