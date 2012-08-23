b719876481e36ce2660a8b6541253860e5b98fa5
paranoiase Kang <paranoiase@gmail.com>
2012-08-23 14:38:02 +0900

    complete. closes #10305

--- app/helpers/groups_helper.rb
+++ app/helpers/groups_helper.rb
@@ -3,4 +3,12 @@ module GroupsHelper
     id = parent ? parent.id : 0
     options_from_collection_for_select(Group.all - [group], 'id', 'name', id)
   end
+
+  def name_with_id(person)
+    if person.employee
+      "#{person.fullname}(#{person.name})"
+    else
+      person.name
+    end
+  end
 end
