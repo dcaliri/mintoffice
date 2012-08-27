b753fe14e855b01bfec07b400f899901df1e8d6e
paranoiase Kang <paranoiase@gmail.com>
2012-08-27 18:15:00 +0900

    add unit test

--- test/unit/bankbook_test.rb
+++ test/unit/bankbook_test.rb
@@ -1,31 +1,31 @@
+# encoding: UTF-8
 require 'test_helper'
 
 class BankBookTest < ActiveSupport::TestCase
-	fixtures :bankbooks
-
-  test "Bankbook should create bankbook" do
-  	bankbook = Bankbook.new
-    bankbook.name = "unit bankbook"
-    bankbook.number = "111-11111-111"
-    bankbook.account_holder = "kim unit"
-    assert bankbook.save!
+  setup do
+    @valid_attributes = {
+      name: "기본 통장",
+      number: "100-100-100010",
+      account_holder: "김 개똥"
+    }
   end
 
-  test "Bankbook should update bankbook" do
-  	bankbook = Bankbook.find(current_bankbook.id)
-    bankbook.name = "update bankbook"
-    bankbook.number = "111-22222-111"
-    bankbook.account_holder = "kim update"
-    assert bankbook.save!
+  test "Bankbook should create bankbook with valid attributes" do
+    bankbook = Bankbook.new(@valid_attributes)
+    assert bankbook.valid?
   end
 
-  test "Bankbook should destroy bankbook" do
-  	bankbook = Bankbook.find(current_bankbook.id)
-    assert bankbook.destroy
-  end
+  test "Bankbook shouldn't create bankbook with invalid attributes" do
+    bankbook = Bankbook.new(@valid_attributes)
+    bankbook.name = nil
+    assert bankbook.invalid?
 
-  private
-  def current_bankbook
-    @bankbook ||= bankbooks(:shinhan)
+    bankbook = Bankbook.new(@valid_attributes)
+    bankbook.number = nil
+    assert bankbook.invalid?
+
+    bankbook = Bankbook.new(@valid_attributes)
+    bankbook.account_holder = nil
+    assert bankbook.invalid?
   end
 end
