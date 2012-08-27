8e1703c4adf62c92161452abbd2b2ee74493d857
paranoiase Kang <paranoiase@gmail.com>
2012-08-27 15:42:37 +0900

    applying group's cardbill generation

--- app/helpers/accessors_helper.rb
+++ app/helpers/accessors_helper.rb
@@ -8,4 +8,9 @@ module AccessorsHelper
 
     options_for_select(collection)
   end
+
+  def find_access_owner(accessor_params)
+    class_name, id = accessor_params.split('-')
+    owner = class_name.classify.constantize.find(id)
+  end
 end
