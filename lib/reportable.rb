04a7898e5366819215bae9d5c06303471f16dfa5
paranoiase Kang <paranoiase@gmail.com>
2012-08-27 18:33:49 +0900

    applying #10422

--- lib/reportable.rb
+++ lib/reportable.rb
@@ -4,6 +4,7 @@ module Reportable
   def create_initial_report
     unless self.report
       report = build_report
+      report.reporters << Person.current_person.reporters.build(report_id: report.id, owner: true)
     end
   end
 
