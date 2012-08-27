29590781be06d57750b502aa693baf5e692039e8
zzilssun <zzilssun@gmail.com>
2012-08-27 15:24:54 +0900

    closes #10368

--- test/unit/document_test.rb
+++ test/unit/document_test.rb
@@ -29,12 +29,20 @@ class DocumentTest < ActiveSupport::TestCase
   end
 
   test "check report text" do
-    def email_notify_title(from)
-      "문서관리 - #{from.fullname}의 결재요청"
+    def email_notify_title(action, doc, from)
+      if action == :rollback
+        "문서관리 - #{doc.title} - #{from.fullname}님에 의해서 반려되었습니다."
+      else
+        "문서관리 - #{from.fullname}의 결재요청"
+    end
     end
 
-    def boxcar_notify_title(from, doc)
-      "문서관리 - #{from.fullname}의 결재요청 : #{doc.title}"
+    def boxcar_notify_title(action, doc, from)
+      if action == :rollback
+        "문서관리 - #{doc.title} - #{from.fullname}님에 의해서 반려되었습니다."
+      else
+        "문서관리 - #{from.fullname}의 결재요청 : #{doc.title}"
+    end
     end
 
     def email_notify_body(doc, url, comment)
@@ -50,11 +58,17 @@ class DocumentTest < ActiveSupport::TestCase
       BODY
     end
 
-    comment_email = "문서관리 - #{current_employee.fullname}의 결재요청"
-    assert_equal(comment_email, email_notify_title(current_employee))
+    report_comment_email = "문서관리 - #{current_employee.fullname}의 결재요청"
+    assert_equal(report_comment_email, email_notify_title(:report, current_document, current_employee))
 
-    comment_boxcar = "문서관리 - #{current_employee.fullname}의 결재요청 : #{current_document.title}"
-    assert_equal(comment_boxcar, boxcar_notify_title(current_employee, current_document))
+    rollback_comment_email = "문서관리 - #{current_document.title} - #{current_employee.fullname}님에 의해서 반려되었습니다."
+    assert_equal(rollback_comment_email, email_notify_title(:rollback, current_document, current_employee))
+
+    report_comment_boxcar = "문서관리 - #{current_employee.fullname}의 결재요청 : #{current_document.title}"
+    assert_equal(report_comment_boxcar, boxcar_notify_title(:report, current_document, current_employee))
+
+    rollback_comment_boxcar = "문서관리 - #{current_document.title} - #{current_employee.fullname}님에 의해서 반려되었습니다."
+    assert_equal(rollback_comment_boxcar, boxcar_notify_title(:rollback, current_document, current_employee))
 
     comment_email_body = "        문서제목 :\n        테스트 문서\n\n        코멘트:\n        test\n\n        링크:\n        /\n"
     assert_equal(comment_email_body, email_notify_body(current_document, '/', 'test'))
