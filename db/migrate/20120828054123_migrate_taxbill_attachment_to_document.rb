2c5c1d499ac9323a02bd8b4d62f25775c137ddb0
paranoiase Kang <paranoiase@gmail.com>
2012-08-30 18:43:28 +0900

    modify migration

--- db/migrate/20120828054123_migrate_taxbill_attachment_to_document.rb
+++ db/migrate/20120828054123_migrate_taxbill_attachment_to_document.rb
@@ -7,23 +7,48 @@ class MigrateTaxbillAttachmentToDocument < ActiveRecord::Migration
       FROM taxbills;
     SQL
 
-    execute <<-SQL
-      UPDATE attachments
-      SET owner_type = "Document", owner_id = (SELECT id
-      FROM documents
-      WHERE documents.owner_type = "Taxbill")
-      WHERE attachments.owner_type = "Taxbill"
-    SQL
+    Attachment.where(owner_type: "Taxbill").find_each do |attachment|
+      taxbill = Taxbill.find(attachment.owner_id)
+      if taxbill
+        document = taxbill.document
+        if document
+          attachment.owner_type = "Document"
+          attachment.owner_id = document.id
+          attachment.save!
+        end
+      end
+    end
+
+    # execute <<-SQL
+    #   UPDATE attachments
+    #   SET owner_type = "Document", owner_id = (SELECT id
+    #   FROM documents
+    #   WHERE documents.owner_type = "Taxbill")
+    #   WHERE attachments.owner_type = "Taxbill"
+    # SQL
   end
 
   def down
-    execute <<-SQL
-      UPDATE attachments
-      SET owner_type = "Taxbill", owner_id = (SELECT owner_id
-      FROM documents
-      WHERE documents.owner_type = "Taxbill")
-      WHERE attachments.owner_type = "Document"
-    SQL
+    # execute <<-SQL
+    #   UPDATE attachments
+    #   SET owner_type = "Taxbill", owner_id = (SELECT owner_id
+    #   FROM documents
+    #   WHERE documents.owner_type = "Taxbill")
+    #   WHERE attachments.owner_type = "Document"
+    # SQL
+
+    Attachment.where(owner_type: "Document").find_each do |attachment|
+      document = Document.find(attachment.owner_id)
+
+      if document
+        taxbill = document.owner
+        if taxbill
+          attachment.owner_type = "Taxbill"
+          attachment.owner_id = taxbill.id
+          attachment.save!
+        end
+      end
+    end
 
     execute <<-SQL
       DELETE FROM documents WHERE documents.owner_type = 'Taxbill'
