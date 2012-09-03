1c645a95e1df0c34af912bdbdef677d614957408
zzilssun <zzilssun@gmail.com>
2012-09-03 18:14:15 +0900

    closes #10647

--- test/functional/contacts_controller_test.rb
+++ test/functional/contacts_controller_test.rb
@@ -2,6 +2,18 @@ require 'test_helper'
 
 class ContactsControllerTest < ActionController::TestCase
   fixtures :contacts
+  fixtures :contact_emails
+  fixtures :contact_email_tags
+  fixtures :contact_email_tags_contact_emails
+  fixtures :contact_addresses
+  fixtures :contact_address_tags
+  fixtures :contact_address_tags_contact_addresses
+  fixtures :contact_phone_numbers
+  fixtures :contact_phone_number_tags
+  fixtures :contact_phone_number_tags_contact_phone_numbers
+  fixtures :contact_others
+  fixtures :contact_other_tags
+  fixtures :contact_other_tags_contact_others
 
   test "should see index page" do
     get :index
@@ -16,6 +28,25 @@ class ContactsControllerTest < ActionController::TestCase
   test "should see show page" do
     get :show, :id => current_contact.id
     assert_response :success
+    assert_select '#descr dl dd a', 3
+
+    ContactAddress.destroy_all
+
+    get :show, :id => current_contact.id
+    assert_response :success
+    assert_select '#descr dl dd a', 2
+
+    ContactEmail.destroy_all
+
+    get :show, :id => current_contact.id
+    assert_response :success
+    assert_select '#descr dl dd a', 1
+
+    ContactPhoneNumber.destroy_all
+
+    get :show, :id => current_contact.id
+    assert_response :success
+    assert_select '#descr dl dd a', 0
   end
 
   test "should see edit page" do
