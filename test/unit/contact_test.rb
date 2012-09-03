082304e993ccf63b48e8c32095c6c86885035590
paranoiase Kang <paranoiase@gmail.com>
2012-09-03 15:34:05 +0900

    fix failed test

--- test/unit/contact_test.rb
+++ test/unit/contact_test.rb
@@ -2,6 +2,7 @@
 require 'test_helper'
 
 class ContactTest < ActiveSupport::TestCase
+  fixtures :companies
   fixtures :people
   fixtures :enrollments
   fixtures :companies
@@ -14,7 +15,7 @@ class ContactTest < ActiveSupport::TestCase
       position: "사장",
       department: "민트기술",
       email_list: nil,
-      person_id: 1,
+      person_id: 0,
       migrated_data: false,
       owner_id: nil,
       isprivate: false,
@@ -22,10 +23,14 @@ class ContactTest < ActiveSupport::TestCase
       google_id: nil,
       google_etag: nil
     }
+
+    Person.current_person = people(:fixture)
   end
 
   test "Contact should create contact with valid attributes" do
+    Company.current_company = Company.first
     contact = Contact.new(@valid_attributes)
+
     assert contact.valid?
   end
 
