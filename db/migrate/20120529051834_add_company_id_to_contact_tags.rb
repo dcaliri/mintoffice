80869676472ddccd24e4fcac4f56bc3e7f002086
paranoiase Kang <paranoiase@gmail.com>
2012-09-03 13:50:05 +0900

    modify company search

--- db/migrate/20120529051834_add_company_id_to_contact_tags.rb
+++ db/migrate/20120529051834_add_company_id_to_contact_tags.rb
@@ -1,7 +1,7 @@
 class AddCompanyIdToContactTags < ActiveRecord::Migration
   def change
     # company = Company.find_or_create_by_name("mintech")
-    company = Company.find_by_name("mintech") || Company.first
+    company = Company.first
     [ContactAddressTag, ContactEmailTag, ContactPhoneNumberTag, ContactOtherTag].each do |tag_class|
       add_column tag_class.to_s.tableize.to_sym, :company_id, :integer
       tag_class.update_all(company_id: company.id)
