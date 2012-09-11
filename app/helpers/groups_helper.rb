a9d2ccc3d95fe247a11dc9e45bfda8ad154676ed
paranoiase Kang <paranoiase@gmail.com>
2012-09-11 16:45:35 +0900

    fix. closes #10834

--- app/helpers/groups_helper.rb
+++ app/helpers/groups_helper.rb
@@ -9,4 +9,12 @@ module GroupsHelper
 
     options_for_select(collection, id)
   end
+
+  def options_for_people_and_group_select
+    people = Person.with_account.without_retired.map{|person| ["[개인] #{person.fullname}(#{person.account.name})", 'person-' + person.id.to_s]}
+    groups = Group.all.map{|group| ["[그룹] #{group.name}", 'group-' + group.id.to_s]}
+    collection = people + groups
+
+    options_for_select(collection)
+  end
 end
