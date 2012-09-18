b956a48099db1589beffd5a4711b0e63f2f8bd47
SungHee Kang <paranoiase@gmail.com>
2012-09-18 18:03:30 +0900

    associate card history to credit card #10874

--- app/models/card_history.rb
+++ app/models/card_history.rb
@@ -4,32 +4,60 @@ class CardHistory < ActiveRecord::Base
   class << self
     def generate
       transaction do
-        CardUsedSource.find_each do |used_source|
-          approved_source = CardApprovedSource.find_by_approve_no(used_source.approve_no)
-
-          if used_source and approved_source
-            history = CardHistory.find_by_approved_number(used_source.approve_no)
-            history = CardHistory.new unless history
-            history.insert_info(used_source, approved_source)
-            history.save!
-          end
-        end
+        generate_shinhan_card_history
+        generate_hyundai_card_history
       end
     end
-  end
 
-  def insert_info(used_source, approved_source)
-    self.creditcard = used_source.creditcard || approved_source.creditcard
+    def generate_shinhan_card_history
+      ShinhanCardUsedHistory.find_each do |used_history|
+        approved_history = ShinhanCardApprovedHistory.find_by_approve_no(used_history.approve_no)
+        next unless used_history and approved_history
 
-    self.transacted_at = approved_source.used_at
-    self.amount = approved_source.money
-    self.amount_local = approved_source.money_foreign
-    self.amount_dollar = approved_source.money_dollar
-    self.country = approved_source.nation
-    self.store_name = approved_source.store_name
-    self.approved_status = approved_source.status
-    self.approved_number = approved_source.approve_no
-    self.paid_at = used_source.approved_at
+        creditcard = Creditcard.find_by_short_name(used_history.card_no)
+        next unless creditcard
+
+        history = CardHistory.find_by_approved_number(used_history.approve_no)
+        history = creditcard.card_histories.build unless history
+
+        history.transacted_at = used_history.approved_at# + used_history.approved_time
+        history.amount = used_history.price
+        history.amount_local = used_history.money_foreign
+        history.amount_dollar = used_history.money_krw
+        history.country = used_history.foreign_country_name
+        history.store_name = used_history.store_name + used_history.store_type + used_history.store_zipcode + used_history.store_addr1 + used_history.store_addr2
+        history.approved_status = approved_history.purchase_status
+        history.approved_number = approved_history.approve_no
+        history.paid_at = approved_history.will_be_paied_at
+
+        history.save!
+      end
+    end
+
+    def generate_hyundai_card_history
+      HyundaiCardUsedHistory.find_each do |used_history|
+        approved_history = HyundaiCardApprovedHistory.find_by_approve_number(used_history.approve_no)
+        next unless used_history and approved_history
+
+        creditcard = Creditcard.find_by_short_name(used_history.card_no)
+        next unless creditcard
+
+        history = CardHistory.find_by_approved_number(used_history.approve_no)
+        history = creditcard.card_histories.build unless history
+
+        history.transacted_at = approved_history.transacted_date# + used_history.transacted_time
+        history.amount = approved_history.money
+        history.amount_local = approved_history.money_us
+        history.amount_dollar = approved_history.money
+        history.country = approved_history.nation
+        history.store_name = approved_history.store_name
+        history.approved_status = approved_history.status
+        history.approved_number = approved_history.approve_number
+        history.paid_at = used_history.approved_at
+
+        history.save!
+      end
+    end
   end
 
   def creditcard_name
