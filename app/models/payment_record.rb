67b5060f038384cd8199620bd771f1972cf8de80
SungHee Kang <paranoiase@gmail.com>
2012-09-24 14:08:43 +0900

    fix. closes #11304

--- app/models/payment_record.rb
+++ app/models/payment_record.rb
@@ -1,20 +1,8 @@
 class PaymentRecord < ActiveRecord::Base
   has_one :payment_request, as: :basis
-  has_one :bankbook, as: :holder
-
-  attr_accessor :bankbook_id
-  before_save :save_bankook
+  belongs_to :bankbook
 
   def generate_payment_request
     PaymentRequest.generate_payment_request(self, self.amount)
-  end
-
-private
-  def save_bankook
-    unless bankbook_id.blank?
-      self.bankbook = Bankbook.find(bankbook_id)
-    else
-      self.bankbook = nil
-    end
   end
 end
