a7df4cb626d79fc530e4278b1b226e07af941961
SungHee Kang <paranoiase@gmail.com>
2012-09-27 14:41:23 +0900

    complete. closes #11428

--- app/models/payroll.rb
+++ app/models/payroll.rb
@@ -4,4 +4,24 @@ class Payroll < ActiveRecord::Base
   belongs_to :employee
   has_many :items, :class_name => "PayrollItem"
   accepts_nested_attributes_for :items, :allow_destroy => :true, :reject_if => REJECT_IF_EMPTY
+
+  def payable
+    items.joins(:payroll_category).merge(PayrollCategory.payable)
+  end
+
+  def deductable
+    items.joins(:payroll_category).merge(PayrollCategory.deductable)
+  end
+
+  def payable_total
+    payable.total rescue 0
+  end
+
+  def deductable_total
+    deductable.total rescue 0
+  end
+
+  def difference_total
+    payable_total - deductable_total
+  end
 end
