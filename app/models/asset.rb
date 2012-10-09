264052a30b0e76c24f9fd11f8655459f3aad85a8
SungHee Kang <paranoiase@gmail.com>
2012-10-09 16:12:37 +0900

    apply change histories of assets #11598

--- app/models/asset.rb
+++ app/models/asset.rb
@@ -1,5 +1,16 @@
+# encoding: UTF-8
+
 class Asset < ActiveRecord::Base
   belongs_to :owner, class_name: 'Employee', foreign_key: 'owner_id'
+
+  include Historiable
+  def history_info
+    {
+      :owner_id => proc do |asset, value|
+        Employee.find_by_id(value).fullname rescue "없음"
+      end
+    }
+  end
 
   def return!
     update_column(:owner_id, nil)
