a3504818500bb7f058feafdded5e39a4a6e30b88
Sooyong Wang <wangsy@wangsy.com>
2013-01-03 00:52:49 +0900

    fixes #12633

--- app/models/used_vacation.rb
+++ app/models/used_vacation.rb
@@ -21,6 +21,12 @@ class UsedVacation < ActiveRecord::Base
     end
   end
 
+  class << self
+    def latest
+      order('"from" DESC')
+    end
+  end
+  
   def title
     "#{self.type.title} : #{self.vacation.employee.fullname}"
   end
