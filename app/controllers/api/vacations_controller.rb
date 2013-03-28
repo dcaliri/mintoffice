375bbaf8c77b5d5915a79a7b40e2d307a0b9e69b
Changshin Lee <iasandcb@gmail.com>
2013-03-28 16:38:37 +0900

    Implement auto-calculation of period.

---
+++ app/controllers/api/vacations_controller.rb
@@ -0,0 +1,12 @@
+module Api
+  class VacationsController < ::ApplicationController
+    skip_before_filter :authorize
+
+    def check_period
+      from = Date.new(params[:from_year].to_i, params[:from_month].to_i, params[:from_day].to_i)
+      to = Date.new(params[:to_year].to_i, params[:to_month].to_i, params[:to_day].to_i)
+      used = UsedVacation.new(from: from, to: to, from_half: params[:from_half], to_half: params[:to_half])
+      render text: "$(used_vacation_period).val('#{used.calculate_period}')"
+    end
+  end
+end
